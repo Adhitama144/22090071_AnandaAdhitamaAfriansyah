7e850e3f0f919a8b0eef88f00935660a37347a55
Adhitama144 <adhitamaafriansyah@gmail.com>
2023-11-27 10:32:43 +0700

    UJIAN AKHIR SEMESTER

--- src/main/java/com/mycompany/_anandaadhitamaa/Main.java
+++ src/main/java/com/mycompany/_anandaadhitamaa/Main.java
@@ -2,15 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package pkg22090071_anandaadhitamaafriansyah;
+package com.mycompany._anandaadhitamaa;
 
 /**
  *
  * @author LENOVO
  */
-public class App {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+public class Main {
+    
 }
